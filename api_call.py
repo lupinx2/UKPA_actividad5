2a34e8412dc8923c283cde686d860d5773dad389
Luis P Ruiz <78879397+lupinx2@users.noreply.github.com>
2023-10-16 17:06:44 -0700

    Remove old code

--- api_call.py
+++ api_call.py
@@ -3,7 +3,6 @@ import requests
 #que este corriendo en el puerto 6660, yo lo probe con Apache2
 url = 'http://127.0.0.1:6660//api_sum.php'
 params = {'num1': 1, 'num2': 2}
-url_with_params = url + "?num1=" + str(params['num1']) + "&num2=" + str(params['num2'])
 response = requests.get(url, params=params)
 print("BEGIN RESULT")
 print(response.text)
